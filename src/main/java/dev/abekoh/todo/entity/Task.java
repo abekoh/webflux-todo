042c4bfa3b3f3a4797e0e9d6958f17c9e930a9c9
Kotaro Abe <abekoh.bcky@gmail.com>
2020-01-05 14:04:11 +0900

    add test

--- src/main/java/dev/abekoh/todo/entity/Task.java
+++ src/main/java/dev/abekoh/todo/entity/Task.java
@@ -1,13 +1,17 @@
 package dev.abekoh.todo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder(toBuilder = true)
 @Table("task")
 public class Task {
---
+++ src/test/java/dev/abekoh/todo/handler/TaskHandler_LTest.java
@@ -0,0 +1,50 @@
+package dev.abekoh.todo.handler;
+
+import dev.abekoh.todo.config.RouteConfiguration;
+import dev.abekoh.todo.entity.Task;
+import dev.abekoh.todo.repository.TaskRepository;
+import dev.abekoh.todo.service.TaskServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = TaskHandler.class)
+@Import({TaskServiceImpl.class, RouteConfiguration.class})
+class TaskHandler_LTest {
+
+    @MockBean
+    TaskRepository repository;
+
+    @Autowired
+    private WebTestClient webClient;
+
+
+    @Test
+    void testGetOne() {
+        Task task = new Task();
+        task.setTaskId(1L);
+        task.setText("やること");
+
+        Mockito.when(repository.getById(1L))
+                .thenReturn(Mono.just(task));
+
+        webClient.get()
+                .uri("/todo/tasks/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.taskId").isEqualTo(1L)
+                .jsonPath("$.text").isEqualTo("やること");
+        
+
+        Mockito.verify(repository, Mockito.times(1)).getById(1L);
+    }
+}
